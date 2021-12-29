0e7cc0a3a0c1560985899c9643c51e343d9f5e8a
Ishrak <irik77587@gmail.com>
2021-12-30 04:39:45 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import { createElement as e } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 ReactDOM.render(e(App, null), document.getElementById("root"));
-module.hot.accept();
+//module.hot.accept();
